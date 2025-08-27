35b0bf37c08d4e13a5e0150eca1800e0d40017a0
Priyanshu <04priyanshuuuu@gmail.com>
2025-08-27 13:48:05 +0530

    added grid and stack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import Herosection from "./components/Herosection";
 import Footer from "./components/Footer";
+import Grid from "./components/Grid";
+import { DraggableCard } from "./components/Stack";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <Herosection />
+      <Grid />
+      <DraggableCard />
       <Footer />
     </main>
   );
