99768072e0480bd492818fa69ba00639f5b9d7fb
Priyanshu <04priyanshuuuu@gmail.com>
2025-10-04 00:56:17 +0530

    completed auth logic

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from accounts.views import UserRegistrationView, UserLoginView
+from accounts.views import UserChangePasswordView, UserPasswordResetView, UserProfileView, UserRegistrationView, UserLoginView,SendPasswordResetEmailView
 
 
 urlpatterns = [
     path("register/", UserRegistrationView.as_view(), name="register"),
     path("login/", UserLoginView.as_view(), name="login"),
+    path("profile/", UserProfileView.as_view(), name="profile"),
+    path("changepassword/", UserChangePasswordView.as_view(), name="changepassword"),
+    path("send-reset-password-email/", SendPasswordResetEmailView.as_view(), name="send-reset-password-email"),
+    path("resetpassword/<uid>/<token>/", UserPasswordResetView.as_view(), name="reset-password"),
 ]
