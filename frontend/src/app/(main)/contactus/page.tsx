2c54a7cd022930e90c55e3180d61d44c2a643aec
Priyanshu <04priyanshuuuu@gmail.com>
2026-03-14 14:37:08 +0530

    fixed contact page code

--- frontend/src/app/(main)/contactus/page.tsx
+++ frontend/src/app/(main)/contactus/page.tsx
@@ -1,11 +1,11 @@
 import WriteToUs from "../home/components/WriteToUs";
-import { Goal } from "./components/Goal";
+import  Goal from "./components/Goal";
 import Hero from "./components/Hero";
 import SocialContacts from "./components/SocialContacts";
 import Team from "./components/Team";
 import TechStack from "./components/TechStack";
 
-function page() {
+function Page() {
   return (
     <div>
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
