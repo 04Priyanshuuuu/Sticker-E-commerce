dd0445aed39fab8f4bfba989476fccfcf382081f
Priyanshu <04priyanshuuuu@gmail.com>
2026-05-18 19:57:32 +0530

    fixed auth.py

--- backend/accounts/authentication.py
+++ backend/accounts/authentication.py
@@ -1,12 +1,60 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken
 
-class CookieJWTAuthentication(JWTAuthentication):
-    def authenticate(self, request):
-        header = self.get_header(request)
-        if header is None:
-            raw_token = request.COOKIES.get('access')
-            if raw_token is None:
+
+class CookieJWTAuthentication(
+    JWTAuthentication
+):
+
+    def authenticate(
+        self,
+        request
+    ):
+
+        header =
+        self.get_header(
+            request
+        )
+
+        if (
+            header is None
+        ):
+
+            raw_token =
+            request.COOKIES.get(
+                "access"
+            )
+
+            if (
+                raw_token is None
+            ):
+
                 return None
-            validated_token = self.get_validated_token(raw_token)
-            return self.get_user(validated_token), validated_token
-        return super().authenticate(request)
+
+            try:
+
+                validated_token = (
+                    self.get_validated_token(
+                        raw_token
+                    )
+                )
+
+                return (
+                    self.get_user(
+                        validated_token
+                    ),
+
+                    validated_token
+                )
+
+            except InvalidToken:
+
+                return None
+
+            except Exception:
+
+                return None
+
+        return super().authenticate(
+            request
+        )
