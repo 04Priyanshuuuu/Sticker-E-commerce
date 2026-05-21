51b670b2ad96646dd13e186572ff5cdc97b8c097
Priyanshu <04priyanshuuuu@gmail.com>
2026-05-21 20:42:13 +0530

    div fix

--- frontend/src/app/(main)/home/components/WriteToUs.tsx
+++ frontend/src/app/(main)/home/components/WriteToUs.tsx
@@ -35,10 +35,12 @@ export default function WriteToUs() {
 
   return (
     <section>
+      <div className="w-full">
+      <h1 className="text-[40px] font-bold m-10 ml-5 border-l-4 border-blue-500 pl-4">
+        Trending
+      </h1>
       <div className="bg-black text-white py-20 flex flex-col items-center px-4">
-        <h1 className="text-[40px] font-bold m-10 ml-5 border-l-4 border-blue-500 pl-4">
-          Trending
-        </h1>
+        
         <h2 className="text-4xl font-bold mb-6">
           Write To Us 📨
         </h2>
@@ -69,6 +71,7 @@ export default function WriteToUs() {
 
         </div>
 
+      </div>
       </div>
     </section>
   );
